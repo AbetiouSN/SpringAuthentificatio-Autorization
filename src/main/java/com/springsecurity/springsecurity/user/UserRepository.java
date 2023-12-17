b919904c7466c724960a6f709bd0ebd06c0b2170
AbetiouSN <sofianeabetiou@gmail.com>
2023-12-17 14:19:15 +0100

    First commit

---
+++ src/main/java/com/springsecurity/springsecurity/user/UserRepository.java
@@ -0,0 +1,12 @@
+package com.springsecurity.springsecurity.user;
+
+import org.hibernate.id.IntegralDataTypeHolder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.OptionalInt;
+
+public interface UserRepository extends JpaRepository<User , Integer> {
+
+    Optional<User> findByEmail(String email);
+}
